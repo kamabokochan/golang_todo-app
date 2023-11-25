df3bf9d1906d15c33e96b48bb413a2a8fa8a38cf
kamabokochan <h.sasaki.worksfront@gmail.com>
2023-11-25 23:15:40 +0900

    Userテーブル作成

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module todo_app
 
 go 1.21.4
 
-require gopkg.in/ini.v1 v1.67.0 // indirect
+require (
+	github.com/mattn/go-sqlite3 v1.14.18 // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
+)
--- src/main.go
+++ src/main.go
@@ -2,15 +2,19 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"todo_app/src/config"
+	"todo_app/src/app/models"
 )
 
 func main() {
-	fmt.Println(config.Config.Port)
-	fmt.Println(config.Config.SQLDriver)
-	fmt.Println(config.Config.DbName)
-	fmt.Println(config.Config.LogFile)
+	// fmt.Println(config.Config.Port)
+	// fmt.Println(config.Config.SQLDriver)
+	// fmt.Println(config.Config.DbName)
+	// fmt.Println(config.Config.LogFile)
 
-	log.Println("test")
+	// log.Println("test")
+
+	fmt.Println(models.Db)
+	// sqlite3 webapp.sql
+	// sqlite> .table
+	// usersと表示される
 }
