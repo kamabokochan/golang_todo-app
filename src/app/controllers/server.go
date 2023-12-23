3157a57c9567a441bf28e76cda69c407c8bb0302
kamabokochan <h.sasaki.worksfront@gmail.com>
2023-12-23 22:19:28 +0900

    css,jsの配信、読み込みの対応

--- src/app/controllers/server.go
+++ src/app/controllers/server.go
@@ -6,6 +6,11 @@ import (
 )
 
 func StartMainServer() error {
-    http.HandleFunc("/", top)
+	// 公開したいディレクトリを指定
+	files := http.FileServer(http.Dir(config.Config.Static))
+	// http.StripPrefix()は、第1引数に指定したパスを、http.FileServer()が捜索するURLから取り除く
+	http.Handle("/static/", http.StripPrefix("/static/", files))
+
+	http.HandleFunc("/", top)
 	return http.ListenAndServe(":"+config.Config.Port, nil)
 }
