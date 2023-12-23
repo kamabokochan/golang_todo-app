50a3311195fa0e014b3b1e39a56cc99a512210e8
kamabokochan <h.sasaki.worksfront@gmail.com>
2023-12-23 22:55:42 +0900

    layout templateの作成と関連した仕組みの修正

--- src/app/controllers/server.go
+++ src/app/controllers/server.go
@@ -1,9 +1,29 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
+	"text/template"
 	"todo_app/src/config"
 )
+
+func generateHTML(w http.ResponseWriter, data interface{}, filenames ...string) {
+	var files []string
+	for _, file := range filenames {
+		files = append(files, fmt.Sprintf("src/app/views/templates/%s.html", file))
+	}
+
+	// MEMO
+	// Must内ParseFilesがエラーの場合は、panicになる
+	// mainを実行する前に落とすことで安全に初期化する際に利用されるらしいが
+	// 今回はmain内で実行されているので、今回の利用目的は要調査。（初期化処理という意味合いで利用している？）一旦講座通りに実装を進める
+	templates := template.Must(template.ParseFiles(files...))
+
+	// MEMO
+	// 指定されたデータオブジェクトに関連付けられているテンプレートを適用し、出力をwrに書き込む
+	// layoutが受け口になるため、明示的にlayoutと指定、dataをlayoutを通し各テンプレートへ読み込ませていると一旦理解
+	templates.ExecuteTemplate(w, "layout", data)
+}
 
 func StartMainServer() error {
 	// 公開したいディレクトリを指定
