7060c048212471db45b3439cfc39522cae389d55
kamabokochan <h.sasaki.worksfront@gmail.com>
2023-12-24 00:22:51 +0900

    formからユーザ情報を送信(POST)、DBに書き込む

--- src/app/controllers/route_auth.go
+++ src/app/controllers/route_auth.go
@@ -1,7 +1,31 @@
 package controllers
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+	"todo_app/src/app/models"
+)
 
+// w: HTTPサーバーがHTTPレスポンスを書き込むためのインターフェース
+// r: HTTPサーバーが受信したHTTPリクエストを表す構造体
 func signup(w http.ResponseWriter, r *http.Request) {
-	generateHTML(w, nil, "layout", "public_navbar", "signup")
+	if r.Method == "GET" {
+		generateHTML(w, nil, "layout", "public_navbar", "signup")
+	} else if r.Method == "POST" {
+		// フォームの値の取得
+		err := r.ParseForm()
+		if err != nil {
+			log.Println(err)
+		}
+		user := models.User{
+			Name:     r.PostFormValue("name"), // formのname属性を渡す
+			Email:    r.PostFormValue("email"),
+			PassWord: r.PostFormValue("password"),
+		}
+		if err := user.CreateUser(); err != nil {
+			log.Println(err)
+		}
+
+		http.Redirect(w, r, "/", 302)
+	}
 }
